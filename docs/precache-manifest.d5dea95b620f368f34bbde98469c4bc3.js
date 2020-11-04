8a8f7a1bdc49b6f8d8b7bc60f4dd096c68ca1d3b
Katelynn <ktlnn.nguyen@gmail.com>
2020-11-04 13:38:27 -0500

    build to docs folder

---
+++ docs/precache-manifest.d5dea95b620f368f34bbde98469c4bc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07532f53bf037995ca35e7564554d589",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "bdf1a95297d23bca28f5",
+    "url": "/react-portfolio/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "c49d7c3e9a71e4de77bb",
+    "url": "/react-portfolio/static/css/main.eac171e7.chunk.css"
+  },
+  {
+    "revision": "bdf1a95297d23bca28f5",
+    "url": "/react-portfolio/static/js/2.6fe4be67.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/react-portfolio/static/js/2.6fe4be67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49d7c3e9a71e4de77bb",
+    "url": "/react-portfolio/static/js/main.99254fc2.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
